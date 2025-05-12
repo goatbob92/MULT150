d739e957b2744d444e30ed61299951a6b39118b1
ALebab904 <98438562+ALebab904@users.noreply.github.com>
2025-05-11 23:58:58 -0700

    Hour 11 | Push 2

--- aschueler_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ aschueler_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+
+    void Start()
+    {
+        Vector3 position = prefab.transform.position;
+        for (int i = 0; i < 10; i++)
+        {
+            position.x += 2;
+            Instantiate(prefab, position, transform.rotation);
+        }
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
