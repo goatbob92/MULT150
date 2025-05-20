30401eb90e6cd262398823e7b3c7e90fdfbf86e3
ALebab904 <98438562+ALebab904@users.noreply.github.com>
2025-05-20 00:40:16 -0700

    Hour 16

---
+++ aschueler_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,11 @@
+/*
+
+- Altered settings of shape to make it more funnel-esk
+- Enabled prewarm
+- Changed start size and size over time
+- Changed velocity over lifetime
+- Changed color
+- Added force over time to make it move upwards but naturally like smoke
+- GOAL to create some smoke effect like from a fire or something
+
+*/
