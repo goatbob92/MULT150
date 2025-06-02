a750b5285a1a75d4eab674f8e1fe99780f40f6c6
ALebab904 <98438562+ALebab904@users.noreply.github.com>
2025-06-02 04:46:47 -0700

    Final

---
+++ aschueler_Final/Assets/Modifications.cs
@@ -0,0 +1,28 @@
+/*
+
+
+
+##     ##  #######  ########  #### ######## ####  ######     ###    ######## ####  #######  ##    ##  ######  
+###   ### ##     ## ##     ##  ##  ##        ##  ##    ##   ## ##      ##     ##  ##     ## ###   ## ##    ## 
+#### #### ##     ## ##     ##  ##  ##        ##  ##        ##   ##     ##     ##  ##     ## ####  ## ##       
+## ### ## ##     ## ##     ##  ##  ######    ##  ##       ##     ##    ##     ##  ##     ## ## ## ##  ######  
+##     ## ##     ## ##     ##  ##  ##        ##  ##       #########    ##     ##  ##     ## ##  ####       ## 
+##     ## ##     ## ##     ##  ##  ##        ##  ##    ## ##     ##    ##     ##  ##     ## ##   ### ##    ## 
+##     ##  #######  ########  #### ##       ####  ######  ##     ##    ##    ####  #######  ##    ##  ######  
+
+
+1 - Changed directional light color to red, intensity to 2
+2 - Added red fog, exponential squared, density 0.04
+3 - Added a red skybox, maximum atmosphere thickness (5), sky tint and ground tint changed, exposure 0.86, no sun
+4 - Modified obstacle to use 3 shapes - Cylinder, capsule, cube, 
+5 - Organized based on parts of the clock (Base, Arrows, Hands)
+6 - Added 2 new textures, icy for clock and snowy for hands, red arrow
+7 - Added particle system for the clock, white and blue, cone shapd, lots of settings changed
+8 - Changed light to range 2, intensity 5, with draw halo checked
+9 - Made Wall 1 a prefab, made Wall 2 a variant of said prefab
+10 - Made a second Wall Material titld Wall Variant, made it purple
+
+
+
+
+*/
---
+++ aschueler_Final/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "GauntletRunner";
+
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
