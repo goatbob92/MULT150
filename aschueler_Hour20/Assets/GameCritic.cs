a82e51b0a361e026769459ac779a6f57e0224a1e
ALebab904 <98438562+ALebab904@users.noreply.github.com>
2025-06-01 23:41:47 -0700

    Hour 20

---
+++ aschueler_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,55 @@
+/*
+
+
+
+ ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▄▄▄▖ ▗▄▄▖▗▄▄▖ ▗▄▄▄▖▗▄▄▄▖▗▄▄▄▖ ▗▄▄▖
+▐▌   ▐▌ ▐▌▐▛▚▞▜▌▐▌   ▐▌   ▐▌ ▐▌  █    █    █  ▐▌   
+▐▌▝▜▌▐▛▀▜▌▐▌  ▐▌▐▛▀▀▘▐▌   ▐▛▀▚▖  █    █    █  ▐▌   
+▝▚▄▞▘▐▌ ▐▌▐▌  ▐▌▐▙▄▄▖▝▚▄▄▖▐▌ ▐▌▗▄█▄▖  █  ▗▄█▄▖▝▚▄▄▖
+                                                   
+1. I would say the game is too easy due to the nature that it is nearly 
+impossible to lose. First of all, the power ups come at quick intervals, 
+meaning it is easy to miss a few and still hold onto a lot of time (10s 
+could be considered too much for this game). Second, in my opinion, the 
+obstacle does the opposite of what it should do. Slowing down the game 
+makes it easy to regain footing if the player is getting overwhelmed by 
+the fast pace, allowing them to quickly regain the lost time through power ups.
+
+2. I think the game could be made harder by making the power ups come less often, or maybe
+putting extra randomization so it isn't always power-up and then obstacle, maybe splitting it
+40% power-ups, 60% obstacles or making obstacles more common as the game goes on. Also,
+ I think an idea that would make the game harder would be to make the obstacles speed up the game
+ instead of slowing it down, and the powerups return the time to normal. This way, if players keep
+ hitting obstacles, they will struggle to capture power-ups, and will lose their time quicker in theory.
+ Another idea would be to just naturally increase the game speed every, say, 10 seconds elapsed, so
+ the obstacls slow it down, but the game gets faster and faster.
+
+3. The first wow factor I would add comes from #2, making the game go faster and faster.
+This gives the game a progressive nature that makes it more engaging, so people won't be able
+to beat it endlessly. I think adding a third power-up that reverses this natural speed increase would
+also be cool, so you can last even longer, however it wouldn't completely negate it.
+The second wow factor I can think of is adding a high score for the player, so they can see their personal best
+and maybe other players' bests to add a competitve challenge style to the game.
+
+4. The game's endless runner mechanic is fun, as you feel like you are moving down a track and making
+progress on it. Also, the game's power-up and obstacle system is fun, as you get a kick out of collecting power-ups 
+and increasing your time left, with the obstacle system "hindering" your ability to progress and survive
+longer.
+
+5. The first tedious part of the game is the obstacle style, as it is both visually unappealing and also
+does the opposite of what it is intended to do, giving a brief pause that makes the game easier rather than 
+punishing the player for hitting it. Also, the power-ups are too frequent, making it easy to last a long time,
+especially since the game speed stays the same, leaving no feel of progression for the player.
+
+
+CHANGES:
+
+OBSTACLE - Changed to a purple color vibe rather than red (to keep villian vibe but emulate
+the idea of slowing down [cool color]), changed model to a sand clock
+
+POWER-UP - Changed to a light green color rather than yellow for the ball. Redesigned
+particle system, made a super cool swirl effect that came about from playing around with 
+different settings, but I really like it and think it could be of good use in future games.
+
+
+*/
